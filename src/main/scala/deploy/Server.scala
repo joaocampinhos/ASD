8ecc798914fcb15301b58d3b6758c1350d6c6764
Flavio Fernandes <flaviofernandes004@gmail.com>
2015-11-10 22:37:46 +0000

    removed unnecessary imports and configuration vars

--- src/main/scala/deploy/Server.scala
+++ src/main/scala/deploy/Server.scala
@@ -1,14 +1,11 @@
 package deploy
 
-import scala.collection.mutable.Set
 import scala.collection.immutable._
-import akka.actor.Props
 import akka.actor.Actor
 import akka.event.Logging
 import akka.actor.ActorRef
 import scala.concurrent.duration._
 import scala.concurrent.forkjoin.ThreadLocalRandom
-import scala.math.Ordering.Implicits._
 
 class Server() extends Actor {
   import context.dispatcher

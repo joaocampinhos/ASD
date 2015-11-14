0a4a5b7a8b27e4342956cf5e066c08f13fc8dbee
Gonçalo Tomás <ga.tomas@outlook.pt>
2015-11-14 23:42:03 +0000

    add view and op definition

--- src/main/scala/paxos/views.scala
+++ src/main/scala/paxos/views.scala
@@ -1,5 +1,11 @@
 package paxos
 
-case class View[T,K](id: Long, participants: List[T], state: List[K] ) {
+import akka.actor.ActorRef
 
-}
+abstract class Operation {}
+
+case class Write(ket: Int, value: String) extends Operation {}
+
+case class Read(key: Int) extends Operation {}
+
+case class View(id: Long, participants: List[ActorRef], state: List[Operation] ) {}

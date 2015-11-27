c118c200a042eff71d2d38466048fe73c49c3580
Flavio Fernandes <flaviofernandes004@gmail.com>
2015-11-27 20:26:39 +0000

    fixed how paxos deploymente. Using classOf.

--- src/main/scala/paxos/Paxos.scala
+++ src/main/scala/paxos/Paxos.scala
@@ -14,152 +14,158 @@ import akka.pattern.ask
 import util.Failure
 import util.Success
 import views.Views.{ View, UpdateView }
+import deploy.Deployer.{ IsReady, Ready }
 
-class Paxos() extends Actor {
-  import context.dispatcher
+object Paxos {
+  case class PaxosConf(id: Int, nActors: Int)
+  class Paxos() extends Actor {
+    import context.dispatcher
 
-  var count: Int = 0
-  val learners = MutableList[ActorRef]()
-  val acceptors = MutableList[ActorRef]()
-  val proposers = MutableList[ActorRef]()
-  var toRespond: MutableList[ActorRef] = MutableList[ActorRef]()
-  var debug = false
-  var done = false
+    var count: Int = 0
+    val learners = MutableList[ActorRef]()
+    val acceptors = MutableList[ActorRef]()
+    val proposers = MutableList[ActorRef]()
+    var toRespond: MutableList[ActorRef] = MutableList[ActorRef]()
+    var debug = false
+    var done = false
 
-  def log(text: Any) = { println(Console.RED + "[" + self.path.name + "] " + Console.GREEN + text + Console.WHITE) }
-  def debugLog(text: Any) = { if (debug) println(Console.RED + "[" + self.path.name + "] " + Console.GREEN + text + Console.WHITE) }
+    def log(text: Any) = { println(Console.RED + "[" + self.path.name + "] " + Console.GREEN + text + Console.WHITE) }
+    def debugLog(text: Any) = { if (debug) println(Console.RED + "[" + self.path.name + "] " + Console.GREEN + text + Console.WHITE) }
 
-  def setupRoles() = {}
+    def setupRoles() = {}
 
-  def receive = {
-    case any: Any => log("Received: " + any)
-  }
-}
-
-class ViewPaxos(id: Int, totalServers: Int) extends Paxos {
-  import context.dispatcher
-  setupRoles()
-  debugLog("Ready")
-  override def setupRoles() = {
-    for (s <- 1 to totalServers) {
-      acceptors += context.actorOf(Props(new Acceptor), name = "acceptor" + s)
+    def receive = {
+      case any: Any => log("Received: " + any)
     }
-    learners += context.actorOf(Props(new Learner(self, totalServers)), name = "learner")
-
-    proposers.foreach(_ ! Servers(acceptors))
-    acceptors.foreach(_ ! Servers(learners))
-
-    // proposers.foreach(_ ! Debug)
-    // acceptors.foreach(_ ! Debug)
-    // learners.foreach(_ ! Debug)
   }
 
-  override def receive = {
-    case Start(UpdateView(k, v)) =>
-      debugLog("Started paxos for a view")
-      toRespond = new MutableList() ++ v.participants
-      toRespond += sender
-      acceptors.foreach(e => e ! Accept(Proposal(1,UpdateView(k, v))))
-
-    case Learn(v) =>
-      count = count + 1
-      if (count == learners.size) {
-        debugLog("Learned: " + v)
-        count = 0
-        for (p <- proposers) {
-          implicit val timeout = Timeout(50 seconds)
-          val future = p ? Stop
-          future onComplete {
-            case Success(result) => debugLog(result)
-            case Failure(failure) => debugLog(failure)
-          }
-        }
-        for (p <- acceptors) {
-          implicit val timeout = Timeout(50 seconds)
-          val future = p ? Stop
-          future onComplete {
-            case Success(result) => debugLog(result)
-            case Failure(failure) => debugLog(failure)
-          }
-        }
-        for (p <- learners) {
-          implicit val timeout = Timeout(50 seconds)
-          val future = p ? Stop
-          future onComplete {
-            case Success(result) => debugLog(result)
-            case Failure(failure) => debugLog(failure)
-          }
-        }
-        for (p <- toRespond) {
-          p ! v
-        }
-        toRespond = new MutableList[ActorRef]()
+  case class ViewPaxos(conf: PaxosConf) extends Paxos {
+    import context.dispatcher
+    setupRoles()
+    debugLog("Ready")
+    override def setupRoles() = {
+      for (s <- 1 to conf.nActors) {
+        acceptors += context.actorOf(Props(new Acceptor), name = "acceptor" + s)
       }
-  }
-}
+      learners += context.actorOf(Props(new Learner(self, conf.nActors)), name = "learner")
 
-class ElectionPaxos(id: Int, totalServers: Int) extends Paxos {
-  import context.dispatcher
-  setupRoles()
-  debugLog("Ready")
-  override def setupRoles() = {
-    for (s <- 1 to totalServers) {
-      acceptors += context.actorOf(Props(new Acceptor), name = "acceptor" + s)
-      proposers += context.actorOf(Props(new Proposer), name = "proposer" + s)
+      proposers.foreach(_ ! Servers(acceptors))
+      acceptors.foreach(_ ! Servers(learners))
+
+      // proposers.foreach(_ ! Debug)
+      // acceptors.foreach(_ ! Debug)
+      // learners.foreach(_ ! Debug)
     }
-    learners += context.actorOf(Props(new Learner(self, totalServers)), name = "learner")
 
-    proposers.foreach(_ ! Servers(acceptors))
-    acceptors.foreach(_ ! Servers(learners))
-
-    // proposers.foreach(_ ! Debug)
-    // acceptors.foreach(_ ! Debug)
-    // learners.foreach(_ ! Debug)
-  }
-
-  override def receive = {
-    case Start(v: Any) =>
-      debugLog("Started paxos for a leader")
-      if (toRespond.size < totalServers) {
+    override def receive = {
+      case IsReady => sender ! Ready
+      case Start(UpdateView(k, v)) =>
+        debugLog("Started paxos for a view")
+        toRespond = new MutableList() ++ v.participants
         toRespond += sender
-        proposers(toRespond.size - 1) ! Operation(v)
-        if (toRespond.size == totalServers) {
-          proposers.foreach(p => p ! Go)
+        acceptors.foreach(e => e ! Accept(Proposal(1, UpdateView(k, v))))
+
+      case Learn(v) =>
+        count = count + 1
+        if (count == learners.size) {
+          debugLog("Learned: " + v)
+          count = 0
+          for (p <- proposers) {
+            implicit val timeout = Timeout(50 seconds)
+            val future = p ? Stop
+            future onComplete {
+              case Success(result) => debugLog(result)
+              case Failure(failure) => debugLog(failure)
+            }
+          }
+          for (p <- acceptors) {
+            implicit val timeout = Timeout(50 seconds)
+            val future = p ? Stop
+            future onComplete {
+              case Success(result) => debugLog(result)
+              case Failure(failure) => debugLog(failure)
+            }
+          }
+          for (p <- learners) {
+            implicit val timeout = Timeout(50 seconds)
+            val future = p ? Stop
+            future onComplete {
+              case Success(result) => debugLog(result)
+              case Failure(failure) => debugLog(failure)
+            }
+          }
+          for (p <- toRespond) {
+            p ! v
+          }
+          toRespond = new MutableList[ActorRef]()
         }
+    }
+  }
+
+  case class ElectionPaxos(conf: PaxosConf) extends Paxos {
+    import context.dispatcher
+    setupRoles()
+    debugLog("Ready")
+    override def setupRoles() = {
+      for (s <- 1 to conf.nActors) {
+        acceptors += context.actorOf(Props(new Acceptor), name = "acceptor" + s)
+        proposers += context.actorOf(Props(new Proposer), name = "proposer" + s)
       }
-    case Learn(v) =>
-      count = count + 1
-      if (count == learners.size) {
-        debugLog("Learned: " + v)
-        count = 0
-        for (p <- proposers) {
-          implicit val timeout = Timeout(50 seconds)
-          val future = p ? Stop
-          future onComplete {
-            case Success(result) => debugLog(result)
-            case Failure(failure) => debugLog(failure)
+      learners += context.actorOf(Props(new Learner(self, conf.nActors)), name = "learner")
+
+      proposers.foreach(_ ! Servers(acceptors))
+      acceptors.foreach(_ ! Servers(learners))
+
+      // proposers.foreach(_ ! Debug)
+      // acceptors.foreach(_ ! Debug)
+      // learners.foreach(_ ! Debug)
+    }
+
+    override def receive = {
+      case IsReady => sender ! Ready
+      case Start(v: Any) =>
+        debugLog("Started paxos for a leader")
+        if (toRespond.size < conf.nActors) {
+          toRespond += sender
+          proposers(toRespond.size - 1) ! Operation(v)
+          if (toRespond.size == conf.nActors) {
+            proposers.foreach(p => p ! Go)
           }
         }
-        for (p <- acceptors) {
-          implicit val timeout = Timeout(50 seconds)
-          val future = p ? Stop
-          future onComplete {
-            case Success(result) => debugLog(result)
-            case Failure(failure) => debugLog(failure)
+      case Learn(v) =>
+        count = count + 1
+        if (count == learners.size) {
+          debugLog("Learned: " + v)
+          count = 0
+          for (p <- proposers) {
+            implicit val timeout = Timeout(50 seconds)
+            val future = p ? Stop
+            future onComplete {
+              case Success(result) => debugLog(result)
+              case Failure(failure) => debugLog(failure)
+            }
           }
-        }
-        for (p <- learners) {
-          implicit val timeout = Timeout(50 seconds)
-          val future = p ? Stop
-          future onComplete {
-            case Success(result) => debugLog(result)
-            case Failure(failure) => debugLog(failure)
+          for (p <- acceptors) {
+            implicit val timeout = Timeout(50 seconds)
+            val future = p ? Stop
+            future onComplete {
+              case Success(result) => debugLog(result)
+              case Failure(failure) => debugLog(failure)
+            }
           }
+          for (p <- learners) {
+            implicit val timeout = Timeout(50 seconds)
+            val future = p ? Stop
+            future onComplete {
+              case Success(result) => debugLog(result)
+              case Failure(failure) => debugLog(failure)
+            }
+          }
+          for (p <- toRespond) {
+            p ! (conf.id, v)
+          }
+          toRespond = new MutableList[ActorRef]()
         }
-        for (p <- toRespond) {
-          p ! (id, v)
-        }
-        toRespond = new MutableList[ActorRef]()
-      }
+    }
   }
 }
